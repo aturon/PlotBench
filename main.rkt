77a703838ec1242d90a394d169770a1034ca049f
Aaron Turon <turon@ccs.neu.edu>
2011-08-23 17:07:57 -0400

    refactored a bit, added 2d plots

--- main.rkt
+++ main.rkt
@@ -3,7 +3,8 @@
 (require racket/list
          racket/match
          racket/vector
-         "../plt-stuff/plot/plot3d.rkt")
+         "../plt-stuff/plot/plot3d.rkt"
+         "../plt-stuff/plot/plot2d.rkt")
 
 (define (normalize-to-first data)
   (define (normalize-vector vec)
@@ -54,41 +55,82 @@
   (define z1 (vector-ref (vector-ref series-data yf) (+ 1 xf)))  
   (+ (* perc-x z1) (* (- 1 perc-x) z0)))
 
-(define (plot-series series-name series-data)
-  (plot3d (surface3d (surface-fn series-data) 
-                     1 7.9999 0 (- (vector-length series-data) 1)
-                     #:color '(100 220 120) 
-                     #:line-color '(100 100 100))
-          #:angle 45 #:altitude 23
-          #:title series-name
-          #:x-label "Threads"
-          #:y-label "log(Work)"
-          #:z-label "Speedup"))
+(define (plot3d-series series-name series-data)
+  (parameterize ((plot3d-font-family 'swiss))
+    (plot3d (surface3d (surface-fn series-data) 
+                       1 7.9999 0 (- (vector-length series-data) 1)
+                       #:color '(100 220 120) 
+                       #:line-color '(100 100 100))
+            #:angle 45 #:altitude 23
+            #:title series-name
+            #:x-label "Threads"
+            #:y-label "log(Work)"
+            #:z-label "Speedup")))
 
-(define (plot-all data)
-  (hash-map data plot-series))
+(define (plot3d-all data)
+  (hash-map data plot3d-series))
 
 (define (load path bench-name date)
   (define file-list (rtp-file-list path bench-name date))
-  (normalize-to-first (load-param-benchmark file-list)))
+  (load-param-benchmark file-list))
 
-(define (load-and-plot path bench-name date)  
-  (plot-all (load path bench-name date)))
+(define (load-and-plot3d path bench-name date)  
+  (plot3d-all (load path bench-name date)))
 
 (define (combine-serii op data1 data2)
-  (for/vector ([i (in-range 0 (vector-length data1))])
-    (for/vector ([j (in-range 0 (vector-length (vector-ref data1 i)))])
+  (for/vector ([i (in-range 0 (min (vector-length data1)
+                                   (vector-length data2)))])
+    (for/vector ([j (in-range 0 (min (vector-length (vector-ref data1 i))
+                                     (vector-length (vector-ref data2 i))))])
       (op (vector-ref (vector-ref data1 i) j)
           (vector-ref (vector-ref data2 i) j)))))
 
-;(load-and-plot "/Users/turon/ChemistrySet/reports" "PushPop" "latest")
-;(load-and-plot "/Users/turon/ChemistrySet/reports" "PushPop" "2011.08.20.18.00.24")
-;(load-and-plot "/Users/turon/ChemistrySet/reports" "IncDec" "2011.08.05.10.15.32")
+(define (plot2d-series series-name series-data)
+  (define colors 
+    (vector 'red 
+            'orange 
+            'yellow 
+            'green
+            'blue 
+            'indigo 
+            'violet))
+  (define param-lines
+    (for/list ([i (in-range 0 (vector-length series-data))])
+      (lines
+       (for/list ([j (in-range 0 (vector-length (vector-ref series-data i)))])
+         (vector (+ 1 j) (vector-ref (vector-ref series-data i) j)))
+       1 8 0 8 #:alpha 0.4 #:width 4
+       #:color (vector-ref colors (modulo i (vector-length colors))))))
+  (plot2d (apply mix param-lines)
+          #:title series-name))
 
-(define data-current (load "/Users/turon/ChemistrySet/reports" "PushPop" "latest"))
-(define data-full (load "/Users/turon/ChemistrySet/reports" "PushPop" "2011.08.20.18.00.24"))
+(define path "/home/turon/ChemistrySet/reports")
 
-(plot-series "Comparison"
-             (combine-serii (lambda (x y) (- y x)) 
-                            (hash-ref data-full "handElim")
+;(load-and-plot3d path "PushPop" "latest")
+;(load-and-plot3d path "PushPop" "2011.08.20.18.00.24")
+;(load-and-plot3d path "IncDec" "2011.08.05.10.15.32")
+
+;(define data-current (load path "PushPop" "latest"))
+(define data-full (load path "PushPop" "2011.08.20.18.00.24"))
+(define data-new-elim (load path "PushPop" "2011.08.23.12.10.26"))
+
+(plot3d-all (normalize-to-first data-full))
+(plot3d-all (normalize-to-first data-new-elim))
+
+(plot3d-series "Reagent-Elim versus Hand-Elim"
+             (combine-serii (lambda (x y) (/ x y)) 
+                            (hash-ref data-new-elim "r-elim")
+                            (hash-ref data-full "handElim")))
+
+(plot3d-series "Reagent-Elim versus Reagent-Teriber"
+             (combine-serii (lambda (x y) (/ x y)) 
+                            (hash-ref data-new-elim "r-elim")
+                            (hash-ref data-full "r-treiber")))
+
+(plot3d-series "Reagent-Elim versus Hand"
+             (combine-serii (lambda (x y) (/ x y)) 
+                            (hash-ref data-new-elim "r-elim")
                             (hash-ref data-full "hand")))
+
+(hash-map (normalize-to-first data-full) plot2d-series)
+(hash-map (normalize-to-first data-new-elim) plot2d-series)
